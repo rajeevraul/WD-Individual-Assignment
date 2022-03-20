63cfa973968e8ae8d592413d84c8da3516042dd9
rajeevraul <rajeev.raul99@gmail.com>
2022-03-20 17:20:09 +0800

    stuff

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
+/*Modal*/
 // Get the modal
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   console.log("working");
   modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -25,10 +21,12 @@ window.onclick = function(event) {
   }
 }
 
+/*URL Locator*/
 function imageClick(url) {
     window.location = url;
 }
 
+/*Pop up alert*/
 function newsletter() {
   if (document.getElementById("footer-email").value == "") {
     alert("Please input a valid email address");
@@ -37,31 +35,54 @@ function newsletter() {
   }
 }
 
-// Set the date we're counting down to
-var countDownDate = new Date("Mar 20, 2022 23:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
+/*Countdown Timer*/
+  // Set the date we're counting down to
+  var countDownDate = new Date("Mar 20, 2022 23:00:00").getTime();
+  // Update the count down every 1 second
+  var x = setInterval(function() {
   // Get today's date and time
   var now = new Date().getTime();
-
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Output the result in an element with id="demo"
+  // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-
-  // If the count down is over, write some text
+  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "IT'S RACE TIME!";
   }
 }, 1000);
+
+//Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
